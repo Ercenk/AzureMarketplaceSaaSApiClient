f1ddc8c7543461bf4d8310018cae0bc55242f725
Ercenk Keresteci <ercenk@microsoft.com>
2019-09-09 18:23:47 +0200

    Fixed the bug for the auth token, ouch!

--- src/AzureAD/AdApplicationHelper.cs
+++ src/AzureAD/AdApplicationHelper.cs
@@ -7,6 +7,7 @@ namespace SaaSFulfillmentClient.AzureAD
     public static class AdApplicationHelper
     {
         private const string authenticationEndpoint = "https://login.microsoftonline.com/";
+
         // Please see https://docs.microsoft.com/en-us/azure/marketplace/partner-center-portal/pc-saas-registration#get-a-token-based-on-the-azure-ad-app
         private const string marketplaceResourceId = "62d94f6c-d599-489b-a797-3e10e42fbe22";
 
@@ -15,7 +16,7 @@ namespace SaaSFulfillmentClient.AzureAD
             var credential = new ClientCredential(options.AzureActiveDirectory.ClientId.ToString(), options.AzureActiveDirectory.AppKey);
             var authContext = new AuthenticationContext(authenticationEndpoint + options.AzureActiveDirectory.TenantId, false);
             var token = await authContext.AcquireTokenAsync(marketplaceResourceId, credential);
-            return $"Bearer {token}";
+            return $"Bearer {token.AccessToken}";
         }
     }
 }
