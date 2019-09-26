e83fb2c25955fd8b439dabe9cef61d61ecc8b33b
Ercenk Keresteci <ercenk@microsoft.com>
2019-09-26 15:36:49 -0700

    Change OperationLocation to operationID

--- src/Models/UpdateOrDeleteSubscriptionRequestResult.cs
+++ src/Models/UpdateOrDeleteSubscriptionRequestResult.cs
@@ -9,7 +9,7 @@ namespace SaaSFulfillmentClient.Models
         private const string OperationLocationKey = "Operation-Location";
         private const string RetryAfterKey = "Retry-After";
 
-        public Uri Operation { get; set; }
+        public Guid Operation { get; set; }
 
         public int RetryAfter { get; set; }
 
@@ -20,20 +20,49 @@ namespace SaaSFulfillmentClient.Models
             if (headers.TryGetValues(OperationLocationKey, out var values))
             {
                 Uri.TryCreate(values.First(), UriKind.Absolute, out var operationUri);
-                this.Operation = operationUri ?? throw new ApplicationException("API did not return an operation ID");
-            }
-
-            if (headers.TryGetValues(RetryAfterKey, out values))
-            {
-                int.TryParse(values.First(), out var retryAfter);
-
-                if (retryAfter == 0)
+                if (operationUri == default)
                 {
-                    throw new ApplicationException("API did not return a retry-after value");
+                    throw new ApplicationException("API did not return an operation ID");
                 }
 
-                this.RetryAfter = retryAfter;
+                // The URI should be like https://marketplaceapi.microsoft.com/api/saas/subscriptions/1be86829-c7ec-1738-ab03-a6cacebe3832/operations/ed10f0b7-6cd6-416d-b015-83c11c9f083b?api-version=2018-08-31
+                // So segments should look like
+                /*
+                    /
+                    api/
+                    saas/
+                    subscriptions/
+                    1be86829-c7ec-1738-ab03-a6cacebe3832/
+                    operations/
+                    ed10f0b7-6cd6-416d-b015-83c11c9f083b
+                */
+
+                if (operationUri.Segments.Length != 7)
+                {
+                    throw new ApplicationException("URI is not recognized as an operation ID url");
+                }
+
+                if (!Guid.TryParse(operationUri.Segments[6], out var operationGuid))
+                {
+                    throw new ApplicationException("Returned operation ID is not a Guid");
+                }
+
+                this.Operation = operationGuid;
             }
+
+            if (!headers.TryGetValues(RetryAfterKey, out values))
+            {
+                return;
+            }
+
+            int.TryParse(values.First(), out var retryAfter);
+
+            if (retryAfter == 0)
+            {
+                throw new ApplicationException("API did not return a retry-after value");
+            }
+
+            this.RetryAfter = retryAfter;
         }
     }
 }
